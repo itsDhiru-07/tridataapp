9f40ed5d087a05e5c9346e24b17b75bb9dddd727
itsDhiru-07 <itsdhirubhai@gmail.com>
2025-06-01 14:44:41 -0400

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-// import DiscoveryPage from './pages/DiscoveryPage';
-// import DesignPage from './pages/DesignPage';
-// import BuildPage from './pages/BuildPage';
-// import OptimizePage from './pages/OptimizePage';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/tridataapp">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/discovery" element={<DiscoveryPage />} />
-        <Route path="/design" element={<DesignPage />} />
-        <Route path="/build" element={<BuildPage />} />
-        <Route path="/optimize" element={<OptimizePage />} /> */}
       </Routes>
     </Router>
   );
